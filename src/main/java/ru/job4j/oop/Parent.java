0c0fe33ae9d2b3e4dc423841c5181d6b0505f869
Артём <iartem1405@gmail.com>
2023-03-18 23:43:17 +0300

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/ru/job4j/oop/Parent.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Parent {
+    private String name;
+
+    private int age;
+
+    public Parent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
