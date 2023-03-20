cea3791c4d978e00f4c6693a88c3411d4b730c38
Артём <iartem1405@gmail.com>
2023-03-20 22:05:37 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,4 +55,18 @@ public class Tracker {
             return false;
         }
     }
+
+    public boolean delete(int id) {
+        if (indexOf(id) != -1) {
+            System.arraycopy(items,
+                    indexOf(id) + 1,
+                    items, indexOf(id),
+                    size - indexOf(id));
+            items[size - 1] = null;
+            size--;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
