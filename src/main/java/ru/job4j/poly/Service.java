d7fb4344bf06e00193f7896f886ab13981b94b1c
Артём <iartem1405@gmail.com>
2023-03-24 21:35:28 +0300

    2.0. Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/poly/Service.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Service {
+    private Store store;
+
+    public Service(Store store) {
+        this.store = store;
+    }
+
+    public void add() {
+        store.save("Petr Arsentev");
+    }
+
+    public static void main(String[] args) {
+        /*FileStore store = new FileStore();
+        MemStore store = new MemStore();
+        Service service = new Service(store);
+        service.add();*/
+    }
+}
