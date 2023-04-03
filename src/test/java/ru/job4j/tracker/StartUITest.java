c9e32cc2fc0ca12a30b856c54a72f88bcb081aeb
Артём <iartem1405@gmail.com>
2023-04-03 22:30:31 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,41 +6,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
 
-    @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-    }
-
-    @Test
-    void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
-        };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("edited item");
-    }
-
-    @Test
-    void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answer = {
-                String.valueOf(item.getId())
-        };
-        StartUI.deleteItem(new StubInput(answer), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted).isNull();
-    }
 }
